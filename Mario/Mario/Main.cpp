0a71505546e1d506e9e20343d3049fd90983dfcf
pavlo.naichuk <pavlo.naichuk@gmail.com>
2017-07-09 16:29:42 +0300

    Adding Mario

---
+++ Mario/Mario/Main.cpp
@@ -0,0 +1,12 @@
+#include "SDL.h"
+
+int main() 
+{
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) 
+	{
+		SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+		return -1;
+	}
+	SDL_Quit();
+	return 0;
+}
