49b39c3d7e08b40adbd416f676425b9aeecf5153
PavloNaichuk <pavlo.naichuk@gmail.com>
2019-04-17 22:05:40 +0300

    Added SocialNetworkWebAPI

--- EpamAcademy/SocialNetwork/SocialNetworkUI/src/app/routes/app-find/app-find.component.ts
+++ EpamAcademy/SocialNetwork/SocialNetworkUI/src/app/routes/app-find/app-find.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../app/services/user.service';
+import { Router } from '@angular/router';
+import { NgForm } from '../../../../node_modules/@angular/forms';
 
 @Component({
   selector: 'app-find',
@@ -6,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app-find.component.css']
 })
 export class AppFindComponent implements OnInit {
-
-  constructor() { }
-
+  submitting = false;
+  formError: string;
+  constructor( private userService:UserService, private router: Router) { }
+  onSubmit(findForm: NgForm): void {
+    if (findForm.valid) {
+      this.submitting = true;
+      this.formError = null;
+    }
+  }
   ngOnInit() {
   }
 
---
+++ EpamAcademy/SocialNetwork/SocialNetworkWebAPI/Controllers/MessageController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace SocialNetworkWebAPI.Controllers
+{
+    [Authorize]
+    public class MessageController : ApiController
+    {
+        // GET api/message
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "messages" };
+        }
+
+        // GET api/message/5
+        public string Get(int id)
+        {
+            return "message";
+        }
+
+        // POST api/message
+        public void Post([FromBody]string message)
+        {
+        }
+
+        // PUT api/message/5
+        public void Put(int id, [FromBody]string message)
+        {
+        }
+
+        // DELETE api/message/5
+        public void Delete(int id)
+        {
+        }
+
+    }
+}
