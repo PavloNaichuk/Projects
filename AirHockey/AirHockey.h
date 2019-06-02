86ee575fb2559edc01c1401c3b90ce88f399373a
PavloNaichuk <pavlo.naichuk@gmail.com>
2019-06-02 08:15:41 +0300

    AirHockey

---
+++ AirHockey/AirHockey.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Common.h"
+#include "GameUnit.h"
+
+struct GameWorld;
+class AirHockey : public GameUnit
+{
+public:
+	AirHockey(State state, const Point& center, const Vector& velocity, const Size& size);
+
+	void ProcessKeyPressed(SDL_Keycode key, GameWorld& gameWorld);
+	void ProcessKeyReleased(SDL_Keycode key);
+	void Update(float elapsedTime);
+	void RemoveLife();
+
+	float mDirection;
+	int mScores;
+	int mLives;
+};
