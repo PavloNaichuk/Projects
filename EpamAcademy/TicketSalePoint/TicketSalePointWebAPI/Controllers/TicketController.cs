7a7581ca24c76038ae6a0685c34cf9a9f456e1b9
PavloNaichuk <pavlo.naichuk@gmail.com>
2019-07-30 06:19:01 +0300

    Fixed ticket ontroller

--- EpamAcademy/TicketSalePoint/TicketSalePointWebAPI/Controllers/TicketController.cs
+++ EpamAcademy/TicketSalePoint/TicketSalePointWebAPI/Controllers/TicketController.cs
@@ -7,15 +7,18 @@ using System.Threading.Tasks;
 
 namespace TicketSalePointWebAPI.Controllers
 {
-    public class TicketsController : ApiController
+    public class TicketController : ApiController
     {
         [HttpGet]
         public async Task<IHttpActionResult> GetAvailableTickets(int showId)
         {
             using (var dataBaseContext = new DataBaseContext())
             {
+
                 var query = from ticket in dataBaseContext.Tickets
-                            select new { ticket.Id, ticket.Row, ticket.Seat, ticket.PriceInCents };
+                            where
+                                ticket.ShowId == showId
+                            select new { ticket.Id, ticket.Row, ticket.Seat, ticket.PriceInCents, ticket.ShowId };
                 return Ok(await query.ToListAsync());
             }
         }
