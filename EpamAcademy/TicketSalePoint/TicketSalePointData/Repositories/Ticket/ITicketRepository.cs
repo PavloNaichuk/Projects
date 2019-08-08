403d2816ce5de5650437c72d226cee0b8e3d6e81
PavloNaichuk <pavlo.naichuk@gmail.com>
2019-08-08 10:14:29 +0300

    Create project and added files

---
+++ EpamAcademy/TicketSalePoint/TicketSalePointData/Repositories/Ticket/ITicketRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TicketSalePointData.Context;
+using TicketSalePoint.Models;
+
+
+namespace TicketSalePoint.Repositories
+{
+    public interface ITicketRepository
+    {
+        Task Add(Ticket ticket);
+        Task Remove(Ticket ticket);
+        Task<Ticket> Query(int id);
+        Task<IEnumerable<Ticket>> GetAvailableTickets(int showId);
+    }
+}
---
+++ EpamAcademy/TicketSalePoint/TicketSalePointWebAPI/Controllers/TheatreController.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TicketSalePoint.Repositories;
+
+namespace TicketSalePoint.Controllers
+{
+    [Route("api/theatre")]
+    [ApiController]
+    public class TheatreController : Controller
+    {
+        private readonly ITheatreRepository theatreRepository;
+
+        public TheatreController(ITheatreRepository theatreRepository)
+        {
+            this.theatreRepository = theatreRepository;
+        }
+
+        public async Task<IActionResult> GetNames()
+        {
+            return Ok(await this.theatreRepository.GetNames());
+        }
+    }
+
+}
