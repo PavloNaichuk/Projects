94a2a586eb3e57f5136bb01eb62079fdb5b882b9
PavloNaichuk <pavlo.naichuk@gmail.com>
2022-01-12 17:37:01 +0200

    Added welcome text in desk

--- Other/BullCowGame/Source/BullCowGame/BullCowCartridge.cpp
+++ Other/BullCowGame/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,11 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine("Wellcome to Bull Cows");
+    PrintLine("Press enter tp continue...");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
+    ClearScreen();
 }
