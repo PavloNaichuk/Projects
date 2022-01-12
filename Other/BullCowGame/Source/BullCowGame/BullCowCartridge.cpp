78b9bdb20949385fb6a2ac156ed8a284df98f2e3
PavloNaichuk <pavlo.naichuk@gmail.com>
2022-01-12 22:24:59 +0200

    Update BullCowCartridge.cpp

--- Other/BullCowGame/Source/BullCowGame/BullCowCartridge.cpp
+++ Other/BullCowGame/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,10 +4,13 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    SetupGame();
+
+    PrintLine(TEXT("The HiddenWord is: %s."), *mHiddenWord); // Debug line
+
     PrintLine(TEXT("Wellcome to Bull Cows"));
-    PrintLine(TEXT("Guess the 4 letter word!")); // Magic Number Remove!
-    PrintLine(TEXT("Press enter to continue..."));
-    SetupGame(); // Setting Up Game
+    PrintLine(TEXT("Guess the %i letter word!"), mHiddenWord.Len());
+    PrintLine(TEXT("Type in your guess and press enter to continue..."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
@@ -22,7 +25,7 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
     {
         if (Input.Len() != mHiddenWord.Len())
         {
-            PrintLine(TEXT("The Hidden Word is 4 characters long, try again!"));
+            PrintLine(TEXT("The Hidden Word is %i characters long, try again!"), mHiddenWord.Len());
         }
 
         PrintLine(TEXT("You have Lost!"));
