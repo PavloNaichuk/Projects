8482b9c6057fd7f416828bfc52a242e36fc12946
PavloNaichuk <pavlo.naichuk@gmail.com>
2022-01-12 21:20:13 +0200

    Added logic

--- Other/BullCowGame/Source/BullCowGame/BullCowCartridge.cpp
+++ Other/BullCowGame/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,11 +4,33 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    PrintLine("Wellcome to Bull Cows");
-    PrintLine("Press enter tp continue...");
+    PrintLine(TEXT("Wellcome to Bull Cows"));
+    PrintLine(TEXT("Guess the 4 letter word!")); // Magic Number Remove!
+    PrintLine(TEXT("Press enter to continue..."));
+    SetupGame(); // Setting Up Game
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
+
+    if (Input == mHiddenWord)
+    {
+        PrintLine(TEXT("You have Won!"));
+    }
+    else
+    {
+        if (Input.Len() != mHiddenWord.Len())
+        {
+            PrintLine(TEXT("The Hidden Word is 4 characters long, try again!"));
+        }
+
+        PrintLine(TEXT("You have Lost!"));
+    }
+}
+
+void UBullCowCartridge::SetupGame()
+{
+    mHiddenWord = TEXT("cake");
+    mLives = 4;
 }
