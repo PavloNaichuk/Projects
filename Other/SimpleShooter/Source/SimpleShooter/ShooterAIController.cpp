3e19faab4175612649cc8e502772c1c23a1b980a
PavloNaichuk <pavlo.naichuk@gmail.com>
2022-01-26 14:25:05 +0200

    Added AI logic

--- Other/SimpleShooter/Source/SimpleShooter/ShooterAIController.cpp
+++ Other/SimpleShooter/Source/SimpleShooter/ShooterAIController.cpp
@@ -4,10 +4,26 @@
 #include "ShooterAIController.h"
 #include "Kismet/GameplayStatics.h"
 
+
 void AShooterAIController::BeginPlay() 
 {
 	Super::BeginPlay();
-	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+}
 
-	SetFocus(PlayerPawn);
+void AShooterAIController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	
+	if (LineOfSightTo(PlayerPawn))
+	{
+		SetFocus(PlayerPawn);
+		MoveToActor(PlayerPawn, AcceptanceRadius);
+	}
+	else
+	{
+		ClearFocus(EAIFocusPriority::Gameplay);
+		StopMovement();
+	}
 }
--- Other/SimpleShooter/Source/SimpleShooter/ShooterAIController.h
+++ Other/SimpleShooter/Source/SimpleShooter/ShooterAIController.h
@@ -13,7 +13,13 @@ UCLASS()
 class SIMPLESHOOTER_API AShooterAIController : public AAIController
 {
 	GENERATED_BODY()
+public:
+	virtual void Tick(float DeltaSeconds) override;
 
 protected:
 	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	float AcceptanceRadius = 200;
 };
