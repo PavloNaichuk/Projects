3a1a1e8535f5c6d4b5dc2f751a8b36b6d74e63a5
PavloNaichuk <pavlo.naichuk@gmail.com>
2022-01-27 10:22:34 +0200

    Create ShooterAIController

--- Other/SimpleShooter/Source/SimpleShooter/ShooterAIController.cpp
+++ Other/SimpleShooter/Source/SimpleShooter/ShooterAIController.cpp
@@ -3,11 +3,20 @@
 
 #include "ShooterAIController.h"
 #include "Kismet/GameplayStatics.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 
 void AShooterAIController::BeginPlay() 
 {
 	Super::BeginPlay();
+	
+	if (AIBehavior != nullptr)
+	{
+		RunBehaviorTree(AIBehavior);
+
+		APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+		GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());
+	}
 }
 
 void AShooterAIController::Tick(float DeltaSeconds)
@@ -15,15 +24,14 @@ void AShooterAIController::Tick(float DeltaSeconds)
 	Super::Tick(DeltaSeconds);
 
 	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
-	
 	if (LineOfSightTo(PlayerPawn))
 	{
-		SetFocus(PlayerPawn);
-		MoveToActor(PlayerPawn, AcceptanceRadius);
+		GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+		GetBlackboardComponent()->SetValueAsVector(TEXT("LastKnownPlayerLocation"), PlayerPawn->GetActorLocation());
 	}
 	else
 	{
-		ClearFocus(EAIFocusPriority::Gameplay);
-		StopMovement();
+		GetBlackboardComponent()->ClearValue(TEXT("PlayerLocation"));
 	}
+
 }
