4fc9d1082fa167d23717be87a28ca23d4e282cb3
PavloNaichuk <pavlo.naichuk@gmail.com>
2022-02-09 11:20:01 +0200

    Update project

--- Other/Shooter/Source/Shooter/Pickup.cpp
+++ Other/Shooter/Source/Shooter/Pickup.cpp
@@ -2,6 +2,10 @@
 
 
 #include "Pickup.h"
+#include "Protagonist.h"
+#include "Kismet/GameplayStatics.h"
+#include "Engine/World.h"
+#include "Sound/SoundCue.h"
 
 APickup::APickup()
 {
@@ -11,6 +15,22 @@ APickup::APickup()
 void APickup::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	Super::OnOverlapBegin(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+	if (OtherActor)
+	{
+		AProtagonist* Protagonist = Cast<AProtagonist>(OtherActor);
+		if (OverlapParticles)
+		{
+			UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), OverlapParticles, GetActorLocation(), FRotator(0.f), true);
+		}
+		if (OverlapSound)
+		{
+			UGameplayStatics::PlaySound2D(this, OverlapSound);
+		}
+
+		Destroy();
+
+		
+	}
 }
 
 void APickup::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
---
+++ Other/Shooter/Source/Shooter/ProtagonistPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ProtagonistPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTER_API AProtagonistPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
