07bb0ecae00de34f915063ca9b4f260e71c19dc4
PavloNaichuk <pavlo.naichuk@gmail.com>
2024-08-31 18:38:34 +0300

    Update UltimateShooterAnimInstance.cpp

--- Other/UltimateShooter/Source/UltimateShooter/UltimateShooterAnimInstance.cpp
+++ Other/UltimateShooter/Source/UltimateShooter/UltimateShooterAnimInstance.cpp
@@ -169,12 +169,12 @@ void UUltimateShooterAnimInstance::TurnInPlace()
 void UUltimateShooterAnimInstance::Lean(float DeltaTime)
 {
 	if (ShooterCharacter == nullptr) return;
-	CharacterYawLastFrame = CharacterYaw;
-	CharacterYaw = ShooterCharacter->GetActorRotation().Yaw;
+	CharacterRotationLastFrame = CharacterRotation;
+	CharacterRotation = ShooterCharacter->GetActorRotation();
 
-	//const FRotator Delta{ UKismetMathLibrary::NormalizedDeltaRotator(CharacterRotation, CharacterRotationLastFrame) };
+	const FRotator Delta{ UKismetMathLibrary::NormalizedDeltaRotator(CharacterRotation, CharacterRotationLastFrame) };
 
-	const float Target{ (CharacterYaw - CharacterYawLastFrame) / DeltaTime };
+	const float Target{ Delta.Yaw / DeltaTime };
 	const float Interp{ FMath::FInterpTo(YawDelta, Target, DeltaTime, 6.0f) };
 	YawDelta = FMath::Clamp(Interp, -90.0f, 90.0f);
 
