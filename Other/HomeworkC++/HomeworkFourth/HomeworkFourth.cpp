79c553863bc818585bd463f3a067110a6ae63748
PavloNaichuk <pavlo.naichuk@gmail.com>
2024-12-11 16:04:11 +0200

    Added homeworkFourth

---
+++ Other/HomeworkC++/HomeworkFourth/HomeworkFourth.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+int maxNumber(int num1, int num2) 
+{ 
+    return (num1 > num2) ? num1 : num2; 
+}
+
+int minNumber(int num1, int num2) 
+{ 
+    return (num1 < num2) ? num1 : num2; 
+}
+
+int main()
+{
+    // Task 1
+    std::cout << "Task 1:\n";
+
+    std::cout << "Enter first number: ";
+    int num1;
+    std::cin >> num1;
+
+    std::cout << "Enter second number: ";
+    int num2;
+    std::cin >> num2;
+
+    std::cout << "Enter third number: ";
+    int num3;
+    std::cin >> num3;
+
+    int maxNum = maxNumber(maxNumber(num1, num2), num3);
+
+    std::cout << "Max number: " << maxNum << "\n";
+
+    // Task 2
+   /* std::cout << "Task 2:\n";
+
+    std::cout << "Enter number: ";
+    int number;
+    std::cin >> number;
+
+    //if ((number % 5 == 0) && (number % 11 == 0))
+       // std::cout << "Divisible by these numbers\n";
+    //else
+       // std::cout << "Indivisible by these numbers\n";
+
+    // Task 3
+    std::cout << "Task 3:\n";
+
+    // Task 4
+    std::cout << "Task 4:\n";*/
+
+    // Task 5
+    std::cout << "Task 5:\n";
+
+    std::cout << "Enter first value: ";
+    int value1;
+    std::cin >> value1;
+
+    std::cout << "Enter second value: ";
+    int value2;
+    std::cin >> value2;
+
+    int minNum = minNumber(value1, value2);
+    std::cout << "Min number: " << minNum << "\n";
+
+    return 0;
+}
