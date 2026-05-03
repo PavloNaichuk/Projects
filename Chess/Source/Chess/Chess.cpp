8e256533b99d224bd373398ee889641f2040805d
PavloNaichuk <pavlo.naichuk@gmail.com>
2026-05-03 08:41:26 +0300

    delete chess

--- Chess/Source/Chess/Chess.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Chess.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Chess, "Chess" );
