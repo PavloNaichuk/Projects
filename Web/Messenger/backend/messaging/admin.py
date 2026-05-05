9c7015eeaaf13dcc762461b16d197978a58157e5
PavloNaichuk <pavlo.naichuk@gmail.com>
2026-05-05 09:07:16 +0300

    Add soft delete for messages

--- Web/Messenger/backend/messaging/admin.py
+++ Web/Messenger/backend/messaging/admin.py
@@ -23,8 +23,23 @@ class MessageAdmin(admin.ModelAdmin):
         "created_at",
         "updated_at",
         "edited_at",
+        "is_deleted",
+        "deleted_at",
         "is_read",
     )
-    list_filter = ("is_read", "created_at", "updated_at", "edited_at")
+    list_filter = (
+        "is_read",
+        "is_deleted",
+        "created_at",
+        "updated_at",
+        "edited_at",
+        "deleted_at",
+    )
     search_fields = ("text", "sender__username", "sender__email")
-    readonly_fields = ("created_at", "updated_at", "edited_at")
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+        "edited_at",
+        "is_deleted",
+        "deleted_at",
+    )
